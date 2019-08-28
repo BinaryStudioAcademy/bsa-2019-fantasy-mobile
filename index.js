914451ff80e219aaf923ea454ef1c02cf9647726
Valdeadmario <vladimir.vstup@gmail.com>
2019-08-28 14:33:09 +0300

    Change settings

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/components/App';
+import Home from './src/index';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Home);
