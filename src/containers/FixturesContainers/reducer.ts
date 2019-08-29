5954e3ddeaea9b2c17e7681a13aa2107796fd17c
Valdeadmario <vladimir.vstup@gmail.com>
2019-08-30 01:53:31 +0300

    Implement Fixtures container

---
+++ src/containers/FixturesContainers/reducer.ts
@@ -0,0 +1,38 @@
+import {
+  SET_GAMEWEEKS,
+  SET_GAMES,
+  SET_IS_LOADING,
+  setGamesAction,
+  setGameweekAction,
+} from './action.type';
+
+import {FixturesItemType, GamesDetailsType} from '../../types/fixtures.types';
+import {GameweekType} from '../../types/gameweek.type';
+
+type State = {
+  gameweeks?: GameweekType[];
+  games?: FixturesItemType[];
+  gameDetails?: GamesDetailsType;
+  isLoading: boolean;
+};
+
+const initialState: State = {isLoading: true};
+
+export default (
+  state = initialState,
+  action: setGamesAction | setGameweekAction,
+) => {
+  switch (action.type) {
+    case SET_GAMEWEEKS:
+      return {...state, gameweeks: action.payload};
+    case SET_GAMES:
+      return {...state, games: action.payload};
+    case SET_IS_LOADING:
+      return {
+        ...state,
+        isLoading: action.payload,
+      };
+    default:
+      return state;
+  }
+};
