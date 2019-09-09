8310a55e37e9de6534ff7cffafe80dc67b25965d
Valdeadmario <vladimir.vstup@gmail.com>
2019-09-09 12:41:13 +0300

    Fix bug with next button is shown on the left part

--- src/containers/FixturesContainers/index.tsx
+++ src/containers/FixturesContainers/index.tsx
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from 'react';
-import {Text, View, ScrollView, ActivityIndicator} from 'react-native';
-import {Text as CustomText, Button, Header} from 'react-native-elements';
-import {connect} from 'react-redux';
-import {bindActionCreators, Dispatch} from 'redux';
+import React, { useState, useEffect } from 'react';
+import { Text, View, ScrollView, ActivityIndicator } from 'react-native';
+import { Text as CustomText, Button, Header } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
 import moment from 'moment';
 
 import {
@@ -10,14 +10,14 @@ import {
   loadGamesAction,
   loadFixtureSubscriptionsAction,
 } from './action';
-import {RootState} from '../../store/types';
-import {FixturesItemType} from '../../types/fixtures.types';
-import {GameweekType} from '../../types/gameweek.type';
+import { RootState } from '../../store/types';
+import { FixturesItemType } from '../../types/fixtures.types';
+import { GameweekType } from '../../types/gameweek.type';
 
 import Fixtures from '../../components/Fixtures/Fixtures';
 import Spinner from '../../components/Spinner';
 
-import {primaryColor, primaryDarkColor} from '../../styles/common';
+import { primaryColor, primaryDarkColor } from '../../styles/common';
 
 type Props = {
   gameweeks: GameweekType[];
@@ -46,13 +46,14 @@ const FixturesContainer = ({
 
   useEffect(() => {
     if (gameweeks) {
-      const gameweek = gameweeks.find(gw => {
+      const gameweek = gameweeks.find((gw) => {
         const now = moment();
         return moment(now).isBefore(gw.end);
       });
       if (gameweek) {
         const gameweekNumber = gameweek.number;
-        setCurrentGameweek(gameweekNumber - 1);
+        // setCurrentGameweek(gameweekNumber - 1);
+        setCurrentGameweek(0);
       }
     }
   }, [gameweeks]);
@@ -65,15 +66,15 @@ const FixturesContainer = ({
 
   if (!games || !gameweeks) {
     return (
-      <View style={{alignItems: 'center', justifyContent: 'center'}}>
-        <ActivityIndicator size="large" color="#0000ff" />
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <ActivityIndicator size='large' color='#0000ff' />
       </View>
     );
   }
   return (
     <View>
       <Header
-        containerStyle={{height: 60, paddingTop: 0}}
+        containerStyle={{ height: 60, paddingTop: 0 }}
         leftComponent={{
           icon: 'menu',
           color: '#fff',
@@ -82,7 +83,7 @@ const FixturesContainer = ({
         }}
         centerComponent={{
           text: 'Fixtures',
-          style: {color: '#fff', fontSize: 20},
+          style: { color: '#fff', fontSize: 20 },
         }}
         backgroundColor={primaryColor}
       />
@@ -95,9 +96,10 @@ const FixturesContainer = ({
             marginTop: 10,
             paddingVertical: 70,
             paddingBottom: 150,
-          }}>
+          }}
+        >
           <CustomText h3>Fixtures page</CustomText>
-          <Text style={{marginVertical: 7, fontSize: 17}}>
+          <Text style={{ marginVertical: 7, fontSize: 17 }}>
             Gameweek {currentGameweek + 1} -{' '}
             {moment(gameweeks[currentGameweek].start).format('ddd D MMMM YYYY')}
           </Text>
@@ -106,24 +108,25 @@ const FixturesContainer = ({
             style={{
               flex: 1,
               flexDirection: 'row',
-              justifyContent: 'space-between',
+              justifyContent: currentGameweek === 0 ? 'flex-end' : 'space-between',
               marginBottom: 15,
               width: '100%',
-            }}>
+            }}
+          >
             {currentGameweek >= 1 && (
-              <View style={{width: '30%', marginLeft: 10}}>
+              <View style={{ width: '30%', marginLeft: 10 }}>
                 <Button
-                  buttonStyle={{backgroundColor: 'green'}}
-                  title="Prev"
+                  buttonStyle={{ backgroundColor: 'green' }}
+                  title='Prev'
                   onPress={() => setCurrentGameweek(currentGameweek - 1)}
                 />
               </View>
             )}
             {currentGameweek < gameweeks.length - 1 && (
-              <View style={{width: '30%', marginRight: 10}}>
+              <View style={{ width: '30%', marginRight: 10, alignSelf: 'flex-end' }}>
                 <Button
-                  buttonStyle={{backgroundColor: 'green'}}
-                  title="Next"
+                  buttonStyle={{ backgroundColor: 'green' }}
+                  title='Next'
                   onPress={() => setCurrentGameweek(currentGameweek + 1)}
                 />
               </View>
@@ -148,8 +151,7 @@ const actions = {
   loadFixtureSubscriptionsAction,
 };
 
-const mapDispatchToProps = (dispatch: Dispatch) =>
-  bindActionCreators(actions, dispatch);
+const mapDispatchToProps = (dispatch: Dispatch) => bindActionCreators(actions, dispatch);
 
 export default connect(
   mapStateToProps,
