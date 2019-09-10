0e4191563d21fd39fd8b99323780fe789245fac5
Vladyslav <fareomg@gmail.com>
2019-09-10 20:43:40 +0300

    Add generateImageSrc() to Sidebar

--- src/helpers/avatar.ts
+++ src/helpers/avatar.ts
@@ -2,7 +2,7 @@ import { User } from '../types/user.type';
 
 export const generateImageSrc = ({ facebook_id, image }: User, imageLink?: string) => {
   let imgSrc =
-    'https://i2.wp.com/www.ahfirstaid.org/wp-content/uploads/2014/07/avatar-placeholder.png';
+    'https://www.trzcacak.rs/myfile/detail/385-3856300_no-avatar-png.png';
   imgSrc = facebook_id
     ? `http://graph.facebook.com/${facebook_id}/picture?type=large`
     : imgSrc;
